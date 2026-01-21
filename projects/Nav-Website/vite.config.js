3b043683d75105d7cd3f38243a3703cda87b24e0
northeast18 <north@Admin.localdomain>
2026-01-21 19:03:18 +0800

    优化响应式布局: 手机端4列，电脑端保持8列

--- projects/Nav-Website/vite.config.js
+++ projects/Nav-Website/vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  base: './',
+  server: {
+    host: '0.0.0.0', // 监听所有网络接口，允许局域网访问
+    port: 5173       // 指定端口号
+  }
 })
